58d1995570c57f6032be659899600144df84f9b8
yazeedhani <haniyazeed9@gmail.com>
2022-08-06 14:03:49 -0600

    Created User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { model, Schema } = require('mongoose')
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    token: String
+},
+{
+    timestamps: true,
+    toObject: {
+        // remove `hashedPassword` field when we call `.toObject`
+        transform: (_doc, user) => {
+            delete user.hashedPassword
+            return user
+        }
+    }
+})
+
+module.exports = model('User', userSchema)
