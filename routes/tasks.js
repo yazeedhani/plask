08d388e1e5340aad9880543aeb13d918c647161f
yazeedhani <haniyazeed9@gmail.com>
2022-08-11 21:11:55 -0600

    Created route to delete a task

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,4 +16,14 @@ router.post('/task/:taskListID', authenticateToken, async (req, res, next) => {
     return res.status(201).json({ task: newTask.toObject() })
 })
 
+router.delete('/task/:taskListID/:taskID', authenticateToken, async (req, res, next) => {
+    const taskListID = req.params.taskListID
+    const taskID = req.params.taskID
+
+    const deletedTask = await Task.findByIdAndDelete(taskID)
+    const updateTaskList = await TaskList.findByIdAndUpdate(taskListID, { $pull: {tasks: taskID} })
+    console.log('UPDATED TASK LIST')
+    return res.status(201).json({taskList: updateTaskList})
+})
+
 module.exports = router
