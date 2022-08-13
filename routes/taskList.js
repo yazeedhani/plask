6b48e8cd7b9f0dd43ec8375318a61c223d957af5
yazeedhani <haniyazeed9@gmail.com>
2022-08-13 15:04:09 -0600

    Created patch and delete routes

--- routes/taskList.js
+++ routes/taskList.js
@@ -6,13 +6,44 @@ const TaskList = require('../models/TaskList')
 
 const authenticateToken = require('../util/authenticateToken')
 
+// GET all task lists
 router.get('/tasklists', authenticateToken, async (req, res, next) => {
 
-    const AllTaskListsForUser = await TaskList.find({owner: req.user_id})
+    const AllTaskListsForUser = await TaskList.find({owner: req.user._id}).populate('tasks')
 
     const AllTaskListsForUserObjects = AllTaskListsForUser.map( taskList => taskList.toObject())
 
     res.status(200).json({AllTaskListsForUserObjects})
+})
+
+// CREATE a task list
+router.post('/tasklists', authenticateToken, (req, res, next) => {
+    req.body.owner = req.user._id
+    TaskList.create(req.body)
+
+    res.status(201).json({taskList: req.body})
+})
+
+// UPDATE a task list
+router.patch('/taskLists/:taskListID', authenticateToken, async (req, res, next) => {
+    const taskListID = req.params.taskListID
+
+    const taskList = await TaskList.findById(taskListID)
+    console.log('TASKLIST: ', taskListID)
+
+    taskList.date = req.body.date
+    taskList.save()
+
+    res.status(201).json({ taskList: taskList})
+})
+
+// DELETE a task list
+router.delete('/tasklists/:id', authenticateToken, async (req, res, next) => {
+    const taskListID = req.params.id
+
+    const deleteTaskList = await TaskList.findByIdAndDelete(taskListID)
+
+    res.status(201).send('Task list deleted')
 })
 
 module.exports = router
